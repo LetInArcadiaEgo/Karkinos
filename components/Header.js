770880a3413603714c5c30f5c2d27fbb8137a144
LetInArcadiaEgo <117809895+LetInArcadiaEgo@users.noreply.github.com>
2024-07-15 13:46:36 +0500

    changes

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,8 @@ export default function Header() {
                 src="./images/troll-face.png" 
                 className="header--image"
             />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h2 className="header--title">Saad's Hell Psyop Meme Generator</h2>
+            <h4 className="header--project">Take over the world with lies and disinfo!</h4>
         </header>
     )
 }
